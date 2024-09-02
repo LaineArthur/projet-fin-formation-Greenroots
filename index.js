f528779b38737464bd853050d4d0967138fc8171
Florian Vrillaud <florian.vrillaud@oclock.school>
2024-09-02 14:46:32 +0200

    Create basic router

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import 'dotenv/config';
 // Import NPM modules
 import express from 'express';
 
+import router from './app/routers/router.js';
+
 // Create Express app
 const app = express();
 
@@ -11,9 +13,11 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+// Configure assets routes (static folder)
+app.use(express.static("./public"));
+
+//Attach all defined routes to the Express application
+app.use(router);
 
 // Start server
 const port = process.env.PORT || 3000;
