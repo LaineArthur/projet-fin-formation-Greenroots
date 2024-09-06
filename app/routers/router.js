5f2afdabd90aff3490f34f5684825ed6b0b4face
LaineArthur <arthur.laine@oclock.school>
2024-09-06 17:00:10 +0200

    Tree controller, modification partial header, more data for database, middleware catchErrors and errorHandlers and view 404 without CSS

--- app/routers/router.js
+++ app/routers/router.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { catchErrors } from '../middlewares/catchErrors.js';
 import homeController from '../controllers/homeController.js';
 import aboutController from '../controllers/aboutController.js';
 import authController from '../controllers/authController.js';
@@ -12,6 +13,17 @@ import supportController from '../controllers/supportController.js';
 // Create a new router instance
 const router = express.Router();
 
-router.get('/', homeController.getHomePage);
+router.get('/', catchErrors(homeController.getHomePage));
+router.get('/nos-arbres', catchErrors(treeController.getAll));
+router.get('/nos-arbres/:slug', catchErrors(treeController.getOne));
+
+// CREATE TREE
+router.post('/mon-espace/gestion-des-arbres', catchErrors(treeController.create));
+
+// UPDATE TREE
+router.patch('/mon-espace/gestion-des-arbres/:slug', catchErrors(treeController.update));
+
+// DELETE TREE
+router.delete('/mon-espace/gestion-des-arbres/:slug', catchErrors(treeController.delete));
 
 export default router;
