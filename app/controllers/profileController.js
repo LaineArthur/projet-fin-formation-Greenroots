fe47cfcf44dd5346c7bb92fa1f5d9d98bf7ec2c9
Nagimova Nouriya <nuriya.nagimova@oclock.school>
2024-09-10 15:02:12 +0200

    <

--- app/controllers/profileController.js
+++ app/controllers/profileController.js
@@ -76,7 +76,6 @@
 // }
     
 // };
-
 //* Functionnality : user profile infos and track orders
 import { User } from'../models/index.js'
 
@@ -85,40 +84,24 @@ import Joi from 'joi';
 export default  { 
     async show (req, res, next) { 
 
-<<<<<<< HEAD
     const id = req.params.id
     const user = await User.findByPk(id)
-=======
-    const user = await User.findByPk(req.params.id)
->>>>>>> 5f4aca80029c7c98dc854619440780dcd497c4a5
     if(!user) {
         return res.status(404).json({ message: "Utilisateur non trouvé"})
     };
     res.render('profil',{ user, title: "GreenRoots - Mon profil", cssFile: "profil.css", bulma: process.env.BULMA_URL });
-<<<<<<< HEAD
 
 
-=======
-   
-    
->>>>>>> 5f4aca80029c7c98dc854619440780dcd497c4a5
     },
 
 
     async update(req, res, next) {
-<<<<<<< HEAD
 
 
 
         const id = Number(req.params.id)
 
 
-=======
-        if(!req.session.id){
-            return res.status(401).json({message: 'utilisateur non authentifie'})
-        }
-        const id = Number(req.session.id)
->>>>>>> 5f4aca80029c7c98dc854619440780dcd497c4a5
         const {lastname, firstname, adress, email, password} = req.body;
 
         const schema = Joi.object({
@@ -156,14 +139,10 @@ export default  {
 
     const updateUser = await User.update({lastname: lastname, firstname: firstname, adress: adress, email: email, password: password});
     res.json(updateUser);
-<<<<<<< HEAD
 
 
 
 
-=======
-    
->>>>>>> 5f4aca80029c7c98dc854619440780dcd497c4a5
     },
 
     async delete(req, res) {
