a9b0e36eb4c6d9327c8b8192bfbaf261f5a73812
LaineArthur <arthur.laine@oclock.school>
2024-09-12 14:48:45 +0200

    Dockerfile and docker compose for deployement

--- app/database/sequelize.js
+++ app/database/sequelize.js
@@ -3,7 +3,8 @@ import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
 function sequelize() {
-    const connexion = new Sequelize(process.env.PG_URL, {
+    const connexion = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
         dialect: 'postgres',
         define: {
             createdAt: 'created_at',
