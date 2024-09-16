fd0d1e1794bd67d9b43270c830f92e6eae1ac0c8
BrunoJubert <jubert.bruno@oclock.school>
2024-09-16 12:10:02 +0200

    adjust burger menu, remove by sroll and click

--- public/js/burgerMenu.js
+++ public/js/burgerMenu.js
@@ -1,11 +1,24 @@
 const icone = document.querySelector('.burger');
+const menu = document.getElementById("burger-menu")
 
 icone.addEventListener("click", function (event) {
     event.preventDefault();
     const menu = document.getElementById("burger-menu");
     menu.classList.toggle("show");
     
-})
+});
+
+document.addEventListener("click", function (event) {
+    if (!menu.contains(event.target) && !icone.contains(event.target)) {
+        menu.classList.remove("show");
+    }
+});
+
+window.addEventListener("scroll", function () {
+    if (menu.classList.contains("show")) {
+        menu.classList.remove("show");
+    }
+});
 
 const notification = document.getElementById('notification');
     
