daa7f58f1b848aeab1ea3dcd9aa5708e2dec3e6a
Nagimova Nouriya <nuriya.nagimova@oclock.school>
2024-09-17 15:31:57 +0200

    Vue admi.ejsn et function delet

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -14,4 +14,14 @@ export default {
         res.render('admin', {message, trees : trees, title: "GreenRoots - Gestion des arbres", cssFile: "tree-management.css", bulma: process.env.BULMA_URL })
     },
 
+
+
+    async delete( req, res, next ) {  
+        const treeId = req.params.id;
+        const tree = await Tree.destroy({ where: { id: treeId }})
+    
+    if(tree ===0) {
+        return next();
+     }
+    },
 }
