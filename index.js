5285daf93afdd56fd3b61ae1b0d0a3e8b6becb90
LaineArthur <arthur.laine@oclock.school>
2024-09-26 18:37:58 +0200

    fix deploy

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// Load environment variables from .env
-import 'dotenv/config';
-
-// Import NPM modules
 import express from 'express';
 import session from 'express-session';
 import RedisStore from 'connect-redis';
 import { createClient } from 'redis';
-
 import router from './app/routers/router.js';
 import { errorHandler, notFound } from './app/middlewares/errorHandlers.js';
 import authMiddleware from './app/middlewares/authMiddleware.js';
@@ -14,17 +9,16 @@ import authMiddleware from './app/middlewares/authMiddleware.js';
 // Create Express app
 const app = express();
 
+
 // Configure view engine
 app.set("views", "./app/views");
 app.set("view engine", "ejs");
 
-// Configure assets routes (static folder)
-app.use("/public", express.static("public"));
-
-//Making parsed data available in 'req.body'
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use("/public", express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
+// Redis client and store setup
 const redisClient = createClient({
   url: process.env.REDIS_URL,
 });
@@ -46,31 +40,24 @@ const store = new RedisStore({
 
 app.use(session({
   store,
-  secret: process.env.SESSION_SECRET, 
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: process.env.NODE_ENV === "production", 
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
-    maxAge: 86400000
-    },
+    maxAge: 1000 * 60 * 60 * 24 // 1 day
+  }
 }));
 
 app.use(authMiddleware);
 
-//Attach all defined routes to the Express application
 app.use(router);
 
-// middleware 404
 app.use(notFound);
-
-// Error handler
 app.use(errorHandler);
 
-
-// Start server
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`GreenRoots app started at http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
